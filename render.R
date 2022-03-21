55e0c6bdf2dd2478bc5fb01992a354f5136ba860
Pete Erickson <peter.m.erickson@gmail.com>
2022-03-21 17:42:25 -0500

    commit

--- render.R
+++ render.R
@@ -6,6 +6,6 @@ options(knitr.table.format="latex")
 
 # knit book
 bookdown::render_book(output_format = "bookdown::pdf_book",
-                      output_dir = "diss_output",
+                      output_dir = "book",
                       output_file = "Erickson_dissertation.pdf")
 
